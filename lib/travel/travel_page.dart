4accd0e052d7a8dbd2096dc19cddc89a1f159df2
Nehal Ingole <nehal712521@gmail.com>
2022-03-25 01:02:29 +0530

    Travel_app

---
+++ lib/travel/travel_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+class TravelPage extends StatefulWidget {
+  const TravelPage({ Key? key }) : super(key: key);
+
+  @override
+  State<TravelPage> createState() => _TravelPageState();
+}
+
+class _TravelPageState extends State<TravelPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
